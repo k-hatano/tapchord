b35869fafd9af0c665a5b66e7f22e586e761c414
k-hatano <kent.ruffle.mgj626@gmail.com>
2014-07-05 00:05:17 +0900

    AudioFormatをMODE_STREAMに、アイコン変更 若干レスポンスが悪くなった気が

--- src/jp/nita/tapchord/Sound.java
+++ src/jp/nita/tapchord/Sound.java
@@ -1,16 +1,17 @@
 package jp.nita.tapchord;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 
 public class Sound {
-	static List<AudioTrack> tracks=new ArrayList<AudioTrack>();
-
+	Integer[] frequencies=new Integer[0];
+	WaveGenerator generator=null;
+	
+	int mode;
+	long term;
+	
 	int volume=0;
 	int sampleRate=4000;
 	int waveform;
@@ -21,101 +22,33 @@ public class Sound {
 	int decay=0;
 	int sustain=0;
 	int release=0;
+	int length=0;
+	
+	int attackLength;
+	int decayLength;
+	int sustainLength;
+	int releaseLength;
+	
+	double sustainLevel;
+	
+	Context context;
 
-	public Sound(Integer[] freqs,Context context){
-		volume=Statics.getValueOfVolume(Statics.getPreferenceValue(context,Statics.PREF_VOLUME,0));
-		soundRange=Statics.getValueOfVolume(Statics.getPreferenceValue(context,Statics.PREF_SOUND_RANGE,0));
-		sampleRate=Statics.getValueOfSamplingRate(Statics.getPreferenceValue(context,Statics.PREF_SAMPLING_RATE,0));
-		waveform=Statics.getPreferenceValue(context,Statics.PREF_WAVEFORM,0);
-
-		attack=Statics.getPreferenceValue(context,Statics.PREF_ATTACK_TIME,0);
-		decay=Statics.getPreferenceValue(context,Statics.PREF_DECAY_TIME,0);
-		sustain=Statics.getPreferenceValue(context,Statics.PREF_SUSTAIN_LEVEL,0)+100;
-		release=Statics.getPreferenceValue(context,Statics.PREF_RELEASE_TIME,0);
-
-		if(tracks.size()>0){
-			for(int j=0;j<tracks.size();j++){
-				tracks.get(j).pause();
-				tracks.get(j).stop();
-				tracks.get(j).release();
-			}
-			tracks=new ArrayList<AudioTrack>();
-		}
-
-		for(int j=0;j<freqs.length;j++){
-			int attackLength=attack*sampleRate/1000;
-			int decayLength=decay*sampleRate/1000;
-			int sustainLength=sampleRate/freqs[j];
-			int releaseLength=release*sampleRate/1000;
-			
-			double sustainLevel=(double)sustain/100.0;
-			
-			int length=attackLength+decayLength+sustainLength+releaseLength;
-
-			AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC,
-					sampleRate,
-					AudioFormat.CHANNEL_CONFIGURATION_MONO,
-					AudioFormat.ENCODING_PCM_16BIT,
-					length*2,
-					AudioTrack.MODE_STATIC);
-
-			short[] wave=new short[length];
-			for(int i=0;i<length;i++){
-				double t=(double)i/sampleRate;
-				double s=wave(t*freqs[j],waveform);
-				double ss=(Short.MAX_VALUE*s*volume/400.0);
-				double sss;
-				if(i<=attackLength){
-					double dst=ss;
-					double dr=(double)i/(double)attackLength;
-					sss=dst*dr;
-				}else if(i<=attackLength+decayLength){
-					double src=(double)ss;
-					double sr=(double)(attackLength+decayLength-i)/(double)decayLength;
-					double dst=(double)ss*sustainLevel;
-					double dr=(double)(i-attackLength)/(double)decayLength;
-					sss=src*sr+dst*dr;
-				}else if(i<=attackLength+decayLength+sustainLength){
-					double src=(double)ss*sustainLevel;
-					sss=src;
-				}else if(i<=attackLength+decayLength+sustainLength+releaseLength){
-					double src=(double)ss*sustainLevel;
-					double sr=(double)(attackLength+decayLength+sustainLength+releaseLength-i)/(double)releaseLength;
-					sss=src*sr;
-				}else{
-					sss=0;
-				}
-				if(sss>=Short.MAX_VALUE) sss=(double)Short.MAX_VALUE;
-				if(sss<=-Short.MAX_VALUE) sss=(double)(-Short.MAX_VALUE);
-				wave[i]=(short)sss;
-			}
-			track.write(wave,0,wave.length);
-			waveLength=wave.length;
-			track.setLoopPoints(attackLength+decayLength,attackLength+decayLength+sustainLength,-1);
-
-			tracks.add(track);
-		}
+	public Sound(Integer[] freqs,Context cont){
+		frequencies=freqs;
+		context=cont;
 	}
 
 	public void play(){
-		for(int j=0;j<tracks.size();j++){
-			tracks.get(j).play();
-		}
+		generator = new WaveGenerator();
+		generator.start();
 	}
 
 	public void stop(){
-		for(int j=0;j<tracks.size();j++){
-			tracks.get(j).stop();
-		}
+		finish();
 	}
 
 	public void release(){
-		for(int j=0;j<tracks.size();j++){
-			tracks.get(j).pause();
-			tracks.get(j).stop();
-			tracks.get(j).release();
-		}
-		tracks=new ArrayList<AudioTrack>();
+		finish();
 	}
 
 	public double wave(double t,int which){
@@ -146,6 +79,66 @@ public class Sound {
 		default:
 			return Math.sin(2.0*Math.PI*t);
 		}
+	}
+
+	class WaveGenerator extends Thread{
+		public void run(){
+			volume=Statics.getValueOfVolume(Statics.getPreferenceValue(context,Statics.PREF_VOLUME,0));
+			soundRange=Statics.getValueOfVolume(Statics.getPreferenceValue(context,Statics.PREF_SOUND_RANGE,0));
+			sampleRate=Statics.getValueOfSamplingRate(Statics.getPreferenceValue(context,Statics.PREF_SAMPLING_RATE,0));
+			waveform=Statics.getPreferenceValue(context,Statics.PREF_WAVEFORM,0);
+
+			attack=Statics.getPreferenceValue(context,Statics.PREF_ATTACK_TIME,0);
+			decay=Statics.getPreferenceValue(context,Statics.PREF_DECAY_TIME,0);
+			sustain=Statics.getPreferenceValue(context,Statics.PREF_SUSTAIN_LEVEL,0)+100;
+			release=Statics.getPreferenceValue(context,Statics.PREF_RELEASE_TIME,0);
+
+			attackLength=attack*sampleRate/1000;
+			decayLength=decay*sampleRate/1000;
+			sustainLength=sampleRate;
+			releaseLength=release*sampleRate/1000;
+
+			sustainLevel=(double)sustain/100.0;
+			
+			length=AudioTrack.getMinBufferSize(sampleRate,
+					AudioFormat.CHANNEL_CONFIGURATION_MONO,
+					AudioFormat.ENCODING_PCM_16BIT);
+			AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC,
+					sampleRate,
+					AudioFormat.CHANNEL_CONFIGURATION_MONO,
+					AudioFormat.ENCODING_PCM_16BIT,
+					sampleRate*2,
+					AudioTrack.MODE_STREAM);
+			mode=0;
+			term=0;
+			track.play();
+			while(mode<=3){
+				track.write(getWave(length),0,length);
+			}
+			track.stop();
+			track.release();
+		}
+
+		public short[] getWave(int length){
+			short[] w = new short[length];
+			for(int i=0;i<length;i++){
+				double s=0;
+				for(int j=0;j<frequencies.length;j++){
+					s+=wave((double)term*frequencies[j]/(double)sampleRate,waveform)*volume/400.0*(Short.MAX_VALUE);
+				}
+				if(s>=Short.MAX_VALUE) s=(double)Short.MAX_VALUE;
+				if(s<=-Short.MAX_VALUE) s=(double)(-Short.MAX_VALUE);
+				w[i]=(short)s;
+				term++;
+				if(term>=sampleRate) term-=sampleRate;
+			}
+			return w;
+		}
+
+	}
+	
+	public void finish(){
+		mode=4;
 	}
 
 }
