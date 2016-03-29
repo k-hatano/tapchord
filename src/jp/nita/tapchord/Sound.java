c79484bdef1eb08b1ad78504b6c9aac607ea6266
kenta.hatano <kenta.hatano@tech.s-cubism.jp>
2016-03-29 12:56:51 +0900

    シェパードトーン実装中(おそらく今のまま使ってはいけない)

--- src/jp/nita/tapchord/Sound.java
+++ src/jp/nita/tapchord/Sound.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.util.Log;
 
 public class Sound {
 	static long tappedTime = 0;
@@ -82,23 +83,60 @@ public class Sound {
 			return t-Math.floor(t)<1.0/4.0?0.5:-0.5;
 		case 5:
 			return t-Math.floor(t)<1.0/8.0?0.5:-0.5;
-		case 6:
-		{
-			double r=0;
-			r+=Math.sin(0.5*Math.PI*t)*gaussian(0.05);
-			r+=Math.sin(1.0*Math.PI*t)*gaussian(0.15);
-			r+=Math.sin(2.0*Math.PI*t)*gaussian(0.6);
-			r+=Math.sin(4.0*Math.PI*t)*gaussian(0.15);
-			r+=Math.sin(8.0*Math.PI*t)*gaussian(0.05);
-			return r;
-		}
 		default:
 			return Math.sin(2.0*Math.PI*t);
 		}
 	}
 	
+	public static double shepardTone(long term,int frequency,int sampleRate,int soundRange,int which) {
+		switch(which) {
+		case 6:
+		{
+			double r=0;
+			
+			double g=0,gg=0;
+			
+			Log.i("Sound","start generate shepard tone wave");
+			
+			g=gaussian((Math.log(frequency)/Math.log(2)) - (soundRange+6-24) + 50);
+			Log.i("Sound","g:"+g);
+			gg+=g;
+			r+=Math.sin(0.5*Math.PI*(double)term*frequency/sampleRate)*g;
+			
+			g=gaussian((Math.log(frequency)/Math.log(2)) - (soundRange+6-12) + 50);
+			Log.i("Sound","g:"+g);
+			gg+=g;
+			r+=Math.sin(1.0*Math.PI*(double)term*frequency/sampleRate)*g;
+			
+			g=gaussian((Math.log(frequency)/Math.log(2)) - (soundRange+6) + 50);
+			Log.i("Sound","g:"+g);
+			gg+=g;
+			r+=Math.sin(2.0*Math.PI*(double)term*frequency/sampleRate)*g;
+			
+			g=gaussian((Math.log(frequency)/Math.log(2)) - (soundRange+6+12) + 50);
+			Log.i("Sound","g:"+g);
+			gg+=g;
+			r+=Math.sin(4.0*Math.PI*(double)term*frequency/sampleRate)*g;
+			
+			g=gaussian((Math.log(frequency)/Math.log(2)) - (soundRange+6+24) + 50);
+			Log.i("Sound","g:"+g);
+			gg+=g;
+			r+=Math.sin(8.0*Math.PI*(double)term*frequency/sampleRate)*g;
+
+			Log.i("Sound","gg:"+gg);
+			
+			return r;
+		}
+		default:
+			return Math.sin(2.0*Math.PI*term*frequency/sampleRate);
+		}
+	}
+	
 	public static double gaussian(double t){
-		return t;
+		double sigma = 1;
+		double mu = 0;
+		Log.i("Sound","t:"+t);
+		return (10)*Math.exp(-(t-mu)*(t-mu)/(2*sigma*sigma));
 	}
 
 	class WaveGenerator extends Thread{
@@ -198,9 +236,17 @@ public class Sound {
 			short[] w = new short[length];
 			for(int i=0;i<length;i++){
 				double s=0;
-				for(int j=0;j<frequencies.length;j++){
-					s+=wave((double)term*frequencies[j]/sampleRate,waveform);
+				switch (waveform) {
+				case 0: case 1: case 2: case 3: case 4: case 5:
+					for(int j=0;j<frequencies.length;j++){
+						s+=wave((double)term*frequencies[j]/sampleRate,waveform);
+					}
+				case 6:
+					for(int j=0;j<frequencies.length;j++){
+						s+=shepardTone(term,frequencies[j],sampleRate,soundRange,waveform);
+					}
 				}
+				
 				s*=volume/400.0*(Short.MAX_VALUE);
 
 				if(enableEnvelope){
