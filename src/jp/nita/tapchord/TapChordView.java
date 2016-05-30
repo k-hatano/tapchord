e5b1fe774d659572da29f5953521de693e24a92b
kenta.hatano <kenta.hatano@tech.s-cubism.jp>
2016-05-30 12:05:13 +0900

    カメラキーを弱く押したときはステータスバーのみリセット

--- src/jp/nita/tapchord/TapChordView.java
+++ src/jp/nita/tapchord/TapChordView.java
@@ -888,6 +888,15 @@ public class TapChordView extends View {
 				scroll = 0;
 				invalidate();
 				break;
+			case KeyEvent.KEYCODE_FOCUS: {
+				for (int i = 0; i < 4; i++) {
+					if (statusbarFlags[i] >= 2) {
+						statusbarFlags[i] = 0;
+					}
+				}
+				invalidate();
+				break;
+			}
 			case KeyEvent.KEYCODE_CAMERA: {
 				boolean statusbarFlag = false;
 				if (scroll != 0) {
