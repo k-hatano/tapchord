dba81d67089f25e079bc2301270e2f51defa2982
kenta.hatano <kenta.hatano@tech.s-cubism.jp>
2016-06-22 14:31:14 +0900

    ACRAが使えるように

--- src/jp/nita/tapchord/TapchordApplication.java
+++ src/jp/nita/tapchord/TapchordApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import org.acra.*;
 import org.acra.annotation.*;
 
-@ReportsCrashes(formKey = "1aWGUyDScQifsvlexBt2Oh0Q1bSi8UnsCFYQartfiEAE", mailTo = "kent.ruffle.mgj626@gmail.com", mode = ReportingInteractionMode.DIALOG, resDialogCommentPrompt = R.string.crashed)
+@ReportsCrashes(formKey = "1Hw5GCMynB_Hs2lQMC6YdG9VjG_QYZxYyvn1N8UwIf-0", mailTo = "kent.ruffle.mgj626@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crashed)
 public class TapchordApplication extends Application {
 	@Override
 	public void onCreate() {
